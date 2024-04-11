6970f9208cb5d2135d51664aee9fef103b41c554
John <patrick.meyerin@gmail.com>
2024-04-11 09:50:53 -0500

    WIP

--- resume/src/main/java/coop/stlma/tech/resume/ResumeApplication.java
+++ resume/src/main/java/coop/stlma/tech/resume/ResumeApplication.java
@@ -1,15 +1,33 @@
 package coop.stlma.tech.resume;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ResumeApplication {
 	public static ApplicationContext applicationContext;
 
+	@Value("${coop.stlma.tech.resume.allowedOrigins}")
+	private String allowedOrigins;
+
 	public static void main(String[] args) {
 		applicationContext = SpringApplication.run(ResumeApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins(allowedOrigins);
+			}
+		};
 	}
 
 }
